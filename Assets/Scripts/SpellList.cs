1847cb2808d09e0a7ecadb246d94e8b2a316274a
Jon Stephens <jstephens@asdfa.net>
2016-01-30 14:14:14 -0700

    shade text, spells listed with space, fix floor border

--- Assets/Scripts/SpellList.cs
+++ Assets/Scripts/SpellList.cs
@@ -6,16 +6,22 @@ public class SpellList : MonoBehaviour {
 	void Start() {
 		var desc = "<b>Spells:</b>\n";
 
+		var lastType = SpellType.Attack;
+
 		foreach (var spell in Codex.spells) {
+			if (spell.type != lastType) desc += "\n";
+
 			desc += spell.name + ": ";
 			foreach (var c in spell.sequence) {
 				desc += Codex.displayChars[c];
 			}
 
 			desc += "\n";
+
+			lastType = spell.type;
 		}
 
-		desc += "Cancel/Change Target: ▲\n";
+		desc += "\nCancel/Change Target: ▲\n";
 
 
 
