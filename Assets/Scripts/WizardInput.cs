5f33c92ad8fdba4a623fa0330acc76d45d8e4675
Randy Slater <rndslater@gmail.com>
2016-01-29 21:16:25 -0700

    I added a wizard input class

---
+++ Assets/Scripts/WizardInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WizardInput : MonoBehaviour {
+
+	public Wizard wizard;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
